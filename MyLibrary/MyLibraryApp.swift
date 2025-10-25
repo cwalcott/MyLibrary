ca42e2029f4362229c9f7491c8508ac0426d2133
Costa Walcott <cwalcott@gmail.com>
2025-10-25 16:15:57 -0400

    New iOS project

---
+++ MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Costa Walcott on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
