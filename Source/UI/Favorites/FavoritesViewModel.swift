5206061a60d3d0960f45caa5673a50012ca81224
Costa Walcott <cwalcott@gmail.com>
2026-01-22 18:29:36 -0500

    Catch errors in books stream

--- Source/UI/Favorites/FavoritesViewModel.swift
+++ Source/UI/Favorites/FavoritesViewModel.swift
@@ -5,6 +5,9 @@ final class FavoritesViewModel: ObservableObject {
     @Published var books: [Book]?
 
     init(database: AppDatabase) {
-        database.books().streamAll().map { $0 as [Book]? }.assign(to: &$books)
+        database.books().streamAll()
+            .map { $0 as [Book]? }
+            .replaceError(with: nil)
+            .assign(to: &$books)
     }
 }
